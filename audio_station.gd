7ed6699a65c2fad4d83aee574d2592b1d045ab4a
boukew99 <boukewestra0@gmail.com>
2022-08-15 16:10:57 +0200

    Implement Advance shuffler

--- audio_station.gd
+++ audio_station.gd
@@ -3,11 +3,14 @@ extends PanelContainer
 onready var player = $AudioStreamPlayer
 onready var list = $VBoxContainer/ItemList
 onready var volume = $VBoxContainer/HBoxContainer/Volume
+onready var advance_shuffle = $VBoxContainer/HBoxContainer/AdvanceShuffle
 
 onready var streams = []
 onready var bus = AudioServer.get_bus_index($AudioStreamPlayer.bus)
 
 var current_index = 0
+var advance = 0
+var rng = RandomNumberGenerator.new()
 var loop = false
 
 func _ready():
@@ -16,6 +19,9 @@ func _ready():
 		var resource = $ResourcePreloader.get_resource(resource_name)
 		streams.append(resource)
 		list.add_item(resource.resource_path.get_file())
+	
+	advance_shuffle.max_value = streams.size() - 2
+	
 	get_tree().connect("files_dropped", self, "_on_files_dropped")
 
 func load_ogg(path):
@@ -26,7 +32,7 @@ func load_ogg(path):
 	stream.data = buffer
 	
 	return stream
-
+	
 func _on_files_dropped(paths, screen):
 	for path in paths:
 		if path.get_extension() == "ogg":
@@ -55,12 +61,16 @@ func _on_Skip_pressed():
 	$AudioStreamPlayer.stop()
 	
 func _on_AudioStreamPlayer_finished():
+	if advance_shuffle.value == 0: #loop
+		_on_ItemList_item_activated(current_index)
+		return
+		
 	list.unselect(current_index) 
 	var selected = list.get_selected_items()
 	if selected:
 		current_index = selected[0]
 	else:
-		current_index = wrapi(current_index + 1, 0, streams.size())
+		current_index = wrapi(current_index + rng.randi_range(1, advance), 0, streams.size())
 		list.select(current_index) 
 		
 	_on_ItemList_item_activated(current_index)
@@ -75,8 +85,8 @@ func _on_FileDialog_files_selected(paths):
 	for path in paths:
 		streams.append(load_ogg(path) )
 		list.add_item(path.get_basename().get_file())
-		
-		
+	
+	advance_shuffle.max_value = streams.size() - 2
 
 func _on_Loop_toggled(button_pressed):
 	loop = button_pressed
@@ -94,12 +104,16 @@ func _on_BandPass_toggled(button_pressed):
 
 
 func _on_SpectrumAnalyzer_toggled(button_pressed):
-	list.visible = not button_pressed
+#	list.visible = not button_pressed
 	$VBoxContainer/SpectrumVisualizer.visible = button_pressed
 	$VBoxContainer/Filters.visible = button_pressed
 
+func _on_AdvanceShuffle_value_changed(value):
+	advance = value
 
 #		var length = player.stream.get_length()
 #		var minutes = length / 60
 #		var seconds = int(length) % 60
 #		list.set_item_tooltip(index, "%2d:%02d" % [minutes, seconds])
+
+
